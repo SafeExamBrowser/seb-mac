1993d13020ba0b5387ccfb069c63bca1f5cd87bd
Daniel R. Schneider <drs@let-alsdock194.ethz.ch>
2012-11-15 17:25:37 +0100

    Added exam preferences panel

---
+++ PrefsExamViewController.m
@@ -0,0 +1,27 @@
+//
+//  PrefsExamViewController.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 15.11.12.
+//
+//
+
+#import "PrefsExamViewController.h"
+
+@interface PrefsExamViewController ()
+
+@end
+
+@implementation PrefsExamViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
