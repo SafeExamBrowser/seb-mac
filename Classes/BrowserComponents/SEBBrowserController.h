cddce06f8157313400ca44b7b0a5369d5812503a
Daniel Schneider <daniel.schneider@let.ethz.ch>
2014-10-06 12:00:10 +0200

    Added BrowserWindowController to git.

---
+++ Classes/BrowserComponents/SEBBrowserController.h
@@ -0,0 +1,13 @@
+//
+//  SEBBrowserController.h
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 06/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SEBBrowserController : NSObject
+
+@end
---
+++ Classes/BrowserComponents/SEBBrowserController.m
@@ -0,0 +1,13 @@
+//
+//  SEBBrowserController.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 06/10/14.
+//
+//
+
+#import "SEBBrowserController.h"
+
+@implementation SEBBrowserController
+
+@end
