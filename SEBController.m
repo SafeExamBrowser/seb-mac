a204a7c1c589ae7b2c3de27739e2f0842de92aec
Daniel Schneider <daniel.schneider@let.ethz.ch>
2014-11-14 13:49:50 +0100

    Changed binding of download and log directory Win textfield to Continuously Updates Value as otherwise value was not accepted when focus wasn't changed before closing prefs. Force install in applications folder now not enforcing when running a debug build.

--- SEBController.m
+++ SEBController.m
@@ -1426,10 +1426,14 @@ bool insideMatrix(){
     if (![self isInApplicationsFolder:currentSEBBundlePath]) {
         // Has SEB to be installed in an Applications folder?
         if ([preferences secureBoolForKey:@"org_safeexambrowser_SEB_forceAppFolderInstall"]) {
+#ifndef DEBUG
             DDLogError(@"Current settings require SEB to be installed in an Applications folder, but it isn't! SEB will therefore quit!");
             _forceAppFolder = YES;
             quittingMyself = TRUE; //SEB is terminating itself
             [NSApp terminate: nil]; //quit SEB
+#else
+            DDLogDebug(@"Current settings require SEB to be installed in an Applications folder, but it isn't! SEB would quit if not Debug build.");
+#endif
         }
     }
     
