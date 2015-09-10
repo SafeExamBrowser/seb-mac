e10c4c8bad7ab5aecb75a2bdd02f8b9da394c969
drs <daniel.schneider@let.ethz.ch>
2015-09-10 11:01:42 +0200

    Adding classes from OS X to iOS SEB build settings.

--- SEB/ViewController.m
+++ SEB/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import <WebKit/WebKit.h>
+#import "Constants.h"
 
 //#import "NSUserDefaults+SEBEncryptedUserDefaults.h"
 
@@ -50,6 +51,7 @@ static NSMutableSet *browserWindowControllers;
 
     // Load start URL from the system's user defaults
     NSString *urlText = @"http://safeexambrowser.org/exams"; //[preferences secureStringForKey:@"org_safeexambrowser_SEB_startURL"];
+//    NSString *urlText = @"https://view.ethz.ch/portal/webclient/index.html#/login";
     
     self.request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlText]];
 
