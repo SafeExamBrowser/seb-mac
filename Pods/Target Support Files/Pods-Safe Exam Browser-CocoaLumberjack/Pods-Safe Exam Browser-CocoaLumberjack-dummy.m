c0a78214da0333e6b0ee233ccf6ac547be80f208
Daniel Schneider <daniel.schneider@let.ethz.ch>
2015-12-30 17:27:09 +0100

    Checked in missing pod files to build without having to install the pod

---
+++ Pods/Target Support Files/Pods-Safe Exam Browser-CocoaLumberjack/Pods-Safe Exam Browser-CocoaLumberjack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Safe_Exam_Browser_CocoaLumberjack : NSObject
+@end
+@implementation PodsDummy_Pods_Safe_Exam_Browser_CocoaLumberjack
+@end
