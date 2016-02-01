59765b44e7d9a0fd9a2007d6f0477897efe534b3
Daniel Schneider <daniel.schneider@let.ethz.ch>
2016-02-01 15:20:12 +0100

    Added new SEBBrowserController (with general BC methods) to repo.

---
+++ Classes/BrowserComponents/SEBBrowserController.h
@@ -0,0 +1,15 @@
+//
+//  SEBBrowserController.h
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 22/01/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SEBBrowserController : NSObject
+
+- (void) createSEBUserAgentFromDefaultAgent:(NSString *)defaultUserAgent;
+
+@end
