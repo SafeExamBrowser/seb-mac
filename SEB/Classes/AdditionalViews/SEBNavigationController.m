942feafcb5cd91333c3ed431754cf3d37754419e
Daniel Schneider <daniel.schneider@let.ethz.ch>
2016-06-11 02:20:41 +0200

    Fixed statusbar appearance.

---
+++ SEB/Classes/AdditionalViews/SEBNavigationController.m
@@ -0,0 +1,46 @@
+//
+//  SEBNavigationController.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 11/06/16.
+//
+//
+
+#import "SEBNavigationController.h"
+
+@interface SEBNavigationController ()
+
+@end
+
+@implementation SEBNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//- (UIStatusBarStyle)preferredStatusBarStyle
+//{
+//    if ([[NSUserDefaults standardUserDefaults] secureIntegerForKey:@"org_safeexambrowser_SEB_mobileStatusBarAppearance"] == mobileStatusBarAppearanceLight) {
+//        return UIStatusBarStyleLightContent;
+//    } else {
+//        return UIStatusBarStyleDefault;
+//    }
+//}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
