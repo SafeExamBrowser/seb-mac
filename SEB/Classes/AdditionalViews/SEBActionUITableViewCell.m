564ea6a76b5b6dcca24c5356692e4d1d976848a5
Daniel Schneider <daniel.schneider@let.ethz.ch>
2017-02-24 17:19:38 +0100

    Fixed build warnings.

--- SEB/Classes/AdditionalViews/SEBActionUITableViewCell.m
+++ SEB/Classes/AdditionalViews/SEBActionUITableViewCell.m
@@ -10,9 +10,6 @@
 
 @implementation SEBActionUITableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
