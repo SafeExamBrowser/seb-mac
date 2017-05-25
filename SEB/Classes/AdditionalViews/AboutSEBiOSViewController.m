73fd24515cba3b263798fc4e8bb9a2413ba1d04e
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2017-05-25 18:10:38 +0200

    SEBMAC-43 Working on adding "About" view to left slider menu with SEB copyright and license information.

---
+++ SEB/Classes/AdditionalViews/AboutSEBiOSViewController.m
@@ -0,0 +1,84 @@
+//
+//  AboutSEBiOSViewController.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 25/05/17.
+//  Copyright (c) 2010-2017 Daniel R. Schneider, ETH Zurich,
+//  Educational Development and Technology (LET),
+//  based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen
+//  Project concept: Thomas Piendl, Daniel R. Schneider, 
+//  Dirk Bauer, Kai Reuter, Tobias Halbherr, Karsten Burger, Marco Lehre, 
+//  Brigitte Schmucki, Oliver Rahs.
+//
+//  ``The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//  http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations
+//  under the License.
+//
+//  The Original Code is SafeExamBrowser for iOS.
+//
+//  The Initial Developer of the Original Code is Daniel R. Schneider.
+//  Portions created by Daniel R. Schneider are Copyright
+//  (c) 2010-2017 Daniel R. Schneider, ETH Zurich, Educational Development
+//  and Technology (LET), based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen. All Rights Reserved.
+//
+//  Contributor(s): ______________________________________.
+//
+
+#import "AboutSEBiOSViewController.h"
+
+
+@implementation AboutSEBiOSViewController
+
+
+- (void)didMoveToParentViewController:(UIViewController *)parent
+{
+    if (parent) {
+        // Add the view to the parent view and position it if you want
+        [[parent view] addSubview:self.view];
+        CGRect viewFrame = parent.view.bounds;
+        [self.view setFrame:viewFrame];
+    } else {
+        [self.view removeFromSuperview];
+    }
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+//    _assistantController = [[SEBInitAssistantViewController alloc] init];
+//    _assistantController.controllerDelegate = self;
+    
+}
+
+
+- (BOOL) prefersStatusBarHidden
+{
+    return true;
+}
+
+
+#pragma mark Delegates
+
+
+-(void)closeAbout
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        _sebViewController.initAssistantOpen = false;
+        [_sebViewController storeNewSEBSettingsSuccessful:nil];
+    }];
+}
+
+
+
+
+@end
