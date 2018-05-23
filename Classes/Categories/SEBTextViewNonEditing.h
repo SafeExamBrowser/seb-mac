91a98a98524cfe4f5bab80defad03a7ecac3ddb3
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2018-05-23 18:27:53 +0200

    SEBMAC-126 Now preventing editing of log messages in lock screen NSTextView.

---
+++ Classes/Categories/SEBTextViewNonEditing.h
@@ -0,0 +1,12 @@
+//
+//  SEBTextViewNonEditing.h
+//  Safe Exam Browser
+//
+//  Created by Daniel R. Schneider on 23.05.18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SEBTextViewNonEditing : NSTextView
+
+@end
