aaba0aae2bfbd1bacb2a83f7c749d9e1b5ef45c5
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2018-08-29 15:14:03 +0200

    SEBMAC-92 Finalized implementation for browser user agent suffix setting option. Updated default browser user agents for other platforms.

--- Classes/BrowserComponents/SEBBrowserController.m
+++ Classes/BrowserComponents/SEBBrowserController.m
@@ -24,13 +24,7 @@
         webKitVersion = SEBUserAgentDefaultSafariVersion;
     }
     
-    // Check if default user agent string contains the browser "Version" number string
-    NSString *versionString = @"";
-    if ([defaultUserAgent rangeOfString:@"Version/"].location == NSNotFound) {
-        versionString = @"Version/10.0 ";
-    }
-
-    defaultUserAgent = [defaultUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@%@/%@", versionString, SEBUserAgentDefaultBrowserSuffix, webKitVersion]];
+    defaultUserAgent = [defaultUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@/%@", SEBUserAgentDefaultBrowserSuffix, webKitVersion]];
     [[MyGlobals sharedMyGlobals] setValue:defaultUserAgent forKey:@"defaultUserAgent"];
 }
 
