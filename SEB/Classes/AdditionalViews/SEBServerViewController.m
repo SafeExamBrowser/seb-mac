43bf7cb7087321b8420c38e277e209598ed05631
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2019-09-04 23:40:15 +0200

    SEBMAC-199 Adding view "Connecting to SEB Server"

---
+++ SEB/Classes/AdditionalViews/SEBServerViewController.m
@@ -0,0 +1,31 @@
+//
+//  SEBServerViewController.m
+//  SafeExamBrowser
+//
+//  Created by Daniel R. Schneider on 04.09.19.
+//
+
+#import "SEBServerViewController.h"
+
+@interface SEBServerViewController ()
+
+@end
+
+@implementation SEBServerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
