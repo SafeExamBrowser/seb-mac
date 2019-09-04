04e2c17f89950eded76355fb22c7302d42f39322
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2019-09-04 19:27:03 +0200

    SEBMAC-199 Process discovery API response properly: Save all received exam API endpoint properties in struct and use those for subsequent API calls.

--- Classes/WebServices/NetworkRequest.swift
+++ Classes/WebServices/NetworkRequest.swift
@@ -30,18 +30,22 @@ extension NetworkRequest {
 }
 
 extension NetworkRequest {
-    fileprivate func load(_ url: URL, httpMethod: String, username: String, password: String, withCompletion completion: @escaping (Model?) -> Void) {
+    fileprivate func load(_ url: URL, httpMethod: String, body: String, username: String, password: String, withCompletion completion: @escaping (Model?) -> Void) {
         let configuration = URLSessionConfiguration.ephemeral
-        let authorizationString = "Basic " + username.data(using: .utf8)!.base64EncodedString() + ":" + password.data(using: .utf8)!.base64EncodedString()
-        configuration.httpAdditionalHeaders = ["Authorization" : authorizationString]
+        let authorizationString = "Basic " + (username + ":" + password).data(using: .utf8)!.base64EncodedString()
+//        configuration.httpAdditionalHeaders = ["Authorization" : authorizationString]
         
         let request = NSMutableURLRequest(url: url)
         request.httpMethod = httpMethod
         request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.addValue(authorizationString, forHTTPHeaderField: "Authorization")
+        request.httpBody = body.data(using: .utf8)!
 
         let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request as URLRequest, completionHandler: { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
             print(data as Any)
+            print(response as Any)
+            print(error as Any)
             guard let receivedData = data else {
                 completion(nil)
                 return
@@ -69,8 +73,8 @@ extension ApiRequest: NetworkRequest {
 		load(resource.url, withCompletion: completion)
 	}
 
-    func load(httpMethod: String, username: String, password: String, completion: @escaping (Resource.Model?) -> Void) {
-        load(resource.url, httpMethod: httpMethod, username: username, password: password, withCompletion: completion)
+    func load(httpMethod: String, body: String, username: String, password: String, completion: @escaping (Resource.Model?) -> Void) {
+        load(resource.url, httpMethod: httpMethod, body: body, username: username, password: password, withCompletion: completion)
     }
 }
 
