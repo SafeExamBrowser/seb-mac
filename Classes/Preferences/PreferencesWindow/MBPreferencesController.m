c5b17dccc01eac0d57dc452ef6c99e43c4d5192f
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2020-03-05 17:00:20 +0100

    SEBMAC-231 Working on: Allow to resize/enlarge Preferences window (most likely only vertically).

--- Classes/Preferences/PreferencesWindow/MBPreferencesController.m
+++ Classes/Preferences/PreferencesWindow/MBPreferencesController.m
@@ -57,7 +57,7 @@ NSString *MBPreferencesSelectionAutosaveKey = @"MBPreferencesSelection";
 - (void)openWindow
 {
     if (!self.window) {
-        PreferencesWindow *prefsWindow = [[PreferencesWindow alloc] initWithContentRect:NSMakeRect(0, 0, 300, 200) styleMask:(NSTitledWindowMask | NSClosableWindowMask) backing:NSBackingStoreBuffered defer:YES];
+        PreferencesWindow *prefsWindow = [[PreferencesWindow alloc] initWithContentRect:NSMakeRect(0, 0, 300, 200) styleMask:(NSTitledWindowMask | NSClosableWindowMask | NSWindowStyleMaskResizable) backing:NSBackingStoreBuffered defer:YES];
         [prefsWindow setReleasedWhenClosed:YES];
         [prefsWindow setShowsToolbarButton:NO];
         //[prefsWindow setLevel:NSModalPanelWindowLevel];
