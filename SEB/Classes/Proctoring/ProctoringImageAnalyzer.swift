f6e8ed224b411c21ff519133d623c54197ba186d
Daniel Schneider <daniel.schneider@let.ethz.ch>
2020-05-23 19:44:51 +0200

    SEBMAC-241 Now also logging if face yaw angle is too big (looking sideways) or no candidate or more than one face is detected.

--- SEB/Classes/Proctoring/ProctoringImageAnalyzer.swift
+++ SEB/Classes/Proctoring/ProctoringImageAnalyzer.swift
@@ -9,35 +9,40 @@ import UIKit
 import Vision
 
 @objc public protocol ProctoringImageAnayzerDelegate {
-    func proctoringEvent(_ proctoringEvent: RemoteProctoringEventType) -> Void
+    func proctoringEvent(_ proctoringEvent: RemoteProctoringEventType, message: String?) -> Void
 }
 
 public class ProctoringImageAnalyzer: NSObject {
  
+    fileprivate var proctoringState = remoteProctoringButtonStateDefault
+    
     @objc weak public var delegate: ProctoringImageAnayzerDelegate?
 
     @objc public func detectFace(in image: CVPixelBuffer) {
         let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
             DispatchQueue.main.async {
                 if let results = request.results as? [VNFaceObservation], results.count > 0 {
-                    print("did detect \(results.count) face(s)")
+//                    print("did detect \(results.count) face(s)")
                     if results.count != 1 {
-                        self.delegate?.proctoringEvent(RemoteProctoringEventTypeError)
+                        self.updateProctoringState(RemoteProctoringEventTypeError, message: "Number of detected faces: \(results.count)")
                     } else {
                         if #available(iOS 12, *) {
-                            let faceRoll = results[0].roll
-                            let faceYaw = results[0].yaw
-                            print("first face roll angle \(String(describing: faceRoll)), yaw angle \(String(describing: faceYaw))")
-                            if faceYaw != nil && abs(faceYaw! as! Double) > 0.2 {
-                                self.delegate?.proctoringEvent(RemoteProctoringEventTypeWarning)
+//                            let faceRoll = results[0].roll
+//                            print("first face roll angle \(String(describing: faceRoll)), yaw angle \(String(describing: faceYaw))")
+                            guard let faceYaw = results.first?.yaw else {
+                                return
+                            }
+                            let faceYawDegrees = self.degrees(radians: faceYaw as! Double)
+                            if abs(faceYawDegrees) > 20 {
+                                self.updateProctoringState(RemoteProctoringEventTypeWarning, message: "Face has an angle of \(faceYawDegrees)")
                                 return
                             }
                         }
                     }
-                    self.delegate?.proctoringEvent(RemoteProctoringEventTypeNormal)
+                    self.updateProctoringState(RemoteProctoringEventTypeNormal, message: "One face detected")
                 } else {
-                    print("did not detect any face")
-                    self.delegate?.proctoringEvent(RemoteProctoringEventTypeError)
+//                    print("did not detect any face")
+                    self.updateProctoringState(RemoteProctoringEventTypeError, message: "No candidate face detected!")
                 }
             }
         })
@@ -45,4 +50,14 @@ public class ProctoringImageAnalyzer: NSObject {
         try? imageRequestHandler.perform([faceDetectionRequest])
     }
     
+    func updateProctoringState(_ proctoringEventType: RemoteProctoringEventType, message: String?) -> Void {
+        if proctoringState != proctoringEventType {
+            proctoringState = proctoringEventType
+            self.delegate?.proctoringEvent(proctoringEventType, message: message)
+        }
+    }
+    
+    func degrees(radians: Double) -> Int {
+        return Int(radians * 180 / .pi)
+    }
 }
