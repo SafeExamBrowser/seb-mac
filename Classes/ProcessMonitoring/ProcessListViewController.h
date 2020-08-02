7364ae7ae633b465c029d52cddaf067ef321a3a3
Daniel Schneider <daniel.schneider@let.ethz.ch>
2020-08-02 23:45:53 +0200

    SEBMAC-247 Now proceeding to start SEB/exam session only after all processes are quit. Added title and message to window with the list of prohibited processes which need to be quit.

--- Classes/ProcessMonitoring/ProcessListViewController.h
+++ Classes/ProcessMonitoring/ProcessListViewController.h
@@ -9,12 +9,24 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol ProcessListViewControllerDelegate <NSObject>
+
+- (void)closeProcessListWindowWithCallback:(id)callback
+                                  selector:(SEL)selector;
+
+@end
+
 @interface ProcessListViewController : NSViewController
+
+@property (weak) id delegate;
 
 @property (strong) IBOutlet NSArrayController *processListArrayController;
 
 @property (strong, nonatomic) NSMutableArray <NSRunningApplication *>*runningApplications;
 @property (strong, nonatomic) NSMutableArray <NSDictionary *>*runningProcesses;
+
+@property (weak, nonatomic) id callback;
+@property (readwrite, nonatomic) SEL selector;
 
 - (void)didTerminateRunningApplications:(NSArray *)terminatedApplications;
 
