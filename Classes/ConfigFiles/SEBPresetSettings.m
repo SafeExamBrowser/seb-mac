41c4a51e1f50200a5f4630a68938ce35f0a28561
Daniel Schneider <daniel.schneider@let.ethz.ch>
2020-08-21 15:58:30 +0200

    SEBMAC-233 Added default prohibited processes.

--- Classes/ConfigFiles/SEBPresetSettings.m
+++ Classes/ConfigFiles/SEBPresetSettings.m
@@ -21,8 +21,64 @@
                           @"identifier" : @"us.zoom.xos",
                       },
                       @{
+                          @"executable" : @"Skype",
+                          @"identifier" : @"com.skype.skype",
+                      },
+                      @{
+                          @"executable" : @"Skype for Business",
+                          @"identifier" : @"com.microsoft.SkypeForBusiness",
+                      },
+                      @{
+                          @"executable" : @"Microsoft Communicator",
+                          @"identifier" : @"com.microsoft.Communicator",
+                      },
+                      @{
+                          @"executable" : @"Microsoft Lync",
+                          @"identifier" : @"com.microsoft.Lync",
+                      },
+                      @{
                           @"executable" : @"Element (Riot)",
                           @"identifier" : @"im.riot.app",
+                      },
+                      @{
+                          @"executable" : @"FaceTime",
+                          @"identifier" : @"com.apple.FaceTime",
+                      },
+                      @{
+                          @"executable" : @"Messages",
+                          @"identifier" : @"com.apple.iChat",
+                      },
+                      @{
+                          @"executable" : @"Telegram",
+                          @"identifier" : @"ru.keepcoder.Telegram",
+                      },
+                      @{
+                          @"executable" : @"GoToMeeting",
+                          @"identifier" : @"com.logmein.GoToMeeting",
+                      },
+                      @{
+                          @"executable" : @"Slack",
+                          @"identifier" : @"com.tinyspeck.slackmacgap",
+                      },
+                      @{
+                          @"executable" : @"Teams",
+                          @"identifier" : @"com.microsoft.teams",
+                      },
+                      @{
+                          @"executable" : @"webexmta",
+                          @"identifier" : @"com.cisco.webex.webexmta",
+                      },
+                      @{
+                          @"executable" : @"Adium",
+                          @"identifier" : @"com.adiumX.adiumX",
+                      },
+                      @{
+                          @"executable" : @"Camtasia*",
+                          @"identifier" : @"com.techsmith.camtasia*",
+                      },
+                      @{
+                          @"executable" : @"Alfred*",
+                          @"identifier" : @"com.runningwithcrayons.Alfred*",
                       },
                   ], // prohibitedProcesses end
               
