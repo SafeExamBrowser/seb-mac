4e55fd2945ec08e90c63093fc905859d07a1b08b
Daniel Schneider <daniel.schneider@let.ethz.ch>
2020-08-24 01:23:18 +0200

    SEBMAC-233 Now preset default prohibited processes cannot be removed anymore in Preferences / Applications / Prohibited Processes. Instead an active preset prohibited process is deactivated, an inactive shows a warning alert.

---
+++ Classes/Preferences/PreferencesWindow/PreferencesViewControllers/ProhibitedProcessesArrayController.h
@@ -0,0 +1,16 @@
+//
+//  ProhibitedProcessesArrayController.h
+//  Safe Exam Browser
+//
+//  Created by Daniel R. Schneider on 23.08.20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProhibitedProcessesArrayController : NSArrayController
+
+@end
+
+NS_ASSUME_NONNULL_END
