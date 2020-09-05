b5ecca358a496361797f57d77e1a42c0ac0507b9
Daniel Schneider <daniel.schneider@let.ethz.ch>
2020-09-05 04:18:05 +0200

    SEBMAC-260 Scroll oversized Preferences panels to top on first display. SEBMAC-259 User Interface Preferences Panel doesn't resize vertically.

--- Classes/Preferences/PreferencesWindow/PreferencesViewControllers/PrefsBrowserViewController.m
+++ Classes/Preferences/PreferencesWindow/PreferencesViewControllers/PrefsBrowserViewController.m
@@ -40,6 +40,7 @@
 @implementation PrefsBrowserViewController
 
 - (void) awakeFromNib {
+    [self scrollToTop:scrollView];
 #ifdef __i386__        // Plugins can't be switched on in the 32-bit Intel build
     [enablePlugIns setEnabled:NO]; // disable the checkbox for plug-ins
 #endif
