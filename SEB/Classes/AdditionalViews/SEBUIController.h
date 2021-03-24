35336dda550e579d7b63ccd1c27b17dfc6de367d
Daniel Schneider <daniel.schneider@let.ethz.ch>
2021-03-24 20:17:59 +0100

    SEBMAC-279 Centralized background tint style determining function (according to statusBarAppearance settings and device type).

--- SEB/Classes/AdditionalViews/SEBUIController.h
+++ SEB/Classes/AdditionalViews/SEBUIController.h
@@ -83,6 +83,7 @@
 
 @property (readwrite) NSUInteger statusBarAppearance;
 @property (readwrite) NSUInteger statusBarAppearanceExtended;
+@property (readwrite) SEBBackgroundTintStyle backgroundTintStyle;
 @property (readwrite) BOOL browserToolbarEnabled;
 @property (readwrite) BOOL dockEnabled;
 
