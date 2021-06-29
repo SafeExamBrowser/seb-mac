6a125f30e6124ea8e9f5816871481856259967db
Daniel R. Schneider <schneider@let.ethz.ch>
2021-06-29 10:45:41 +0200

    SEBMAC-327 Merge SEB macOS 2.3.2 into 3.0_unified: Updated some settings for iOS target (doesn't build yet).

--- Pods/Target Support Files/Pods-Safe Exam Browser/Pods-Safe Exam Browser-frameworks.sh
+++ Pods/Target Support Files/Pods-Safe Exam Browser/Pods-Safe Exam Browser-frameworks.sh
@@ -175,10 +175,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack-macOS/CocoaLumberjack.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack-framework-macOS/CocoaLumberjack.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack-macOS/CocoaLumberjack.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack-framework-macOS/CocoaLumberjack.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
