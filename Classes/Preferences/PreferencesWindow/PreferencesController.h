9989d5e0b64495569d67396a65a5a32c116e6afa
Daniel R. Schneider <schneider@let.ethz.ch>
2021-09-17 13:55:26 +0200

    SEBMAC-327 Now applying/asking to apply changed settings also when Teach URL Filter feature in toggled in Preferences and no other settings are changed.

--- Classes/Preferences/PreferencesWindow/PreferencesController.h
+++ Classes/Preferences/PreferencesWindow/PreferencesController.h
@@ -69,6 +69,7 @@
     SEBEncapsulatedSettings *settingsBeforeEditing;
     BOOL restartSEB;
     NSURL *currentSEBFileURL;
+    BOOL urlFilterLearningModeInitialState;
 }
 
 @property BOOL currentConfigPasswordIsHash;
