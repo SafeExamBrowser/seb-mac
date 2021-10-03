5a1e4a6287ca3cb8244100db9079c6cc7996a07f
Daniel R. Schneider <schneider@let.ethz.ch>
2021-10-04 00:41:12 +0200

    SEBMAC-327 Again using local time zone instead of UTC+0 for entries in log files.

--- SEB/SEBViewController.m
+++ SEB/SEBViewController.m
@@ -251,10 +251,7 @@ static NSMutableSet *browserWindowControllers;
         }
     } else {
         if (!_myLogger) {
-            DDLogFileManagerDefault* logFileManager = [[DDLogFileManagerDefault alloc] init];
-            _myLogger = [[DDFileLogger alloc] initWithLogFileManager:logFileManager];
-            _myLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
-            _myLogger.logFileManager.maximumNumberOfLogFiles = 7; // keep logs for 7 days
+            _myLogger = [MyGlobals initializeFileLoggerWithDirectory:nil];
             [DDLog addLogger:_myLogger];
         }
         if ([preferences secureIntegerForKey:@"org_safeexambrowser_SEB_sebMode"] == sebModeSebServer) {
