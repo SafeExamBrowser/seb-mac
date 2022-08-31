f82612e193f6cc40c3aabf16f8b937ac3ee5d6e5
Daniel R. Schneider <danschlet@users.noreply.github.com>
2022-08-31 17:50:18 +0200

    SEBMAC-430 Fixed quitting running applications when starting SEB in non-debug mode.

--- SEBVerificator/ViewController.swift
+++ SEBVerificator/ViewController.swift
@@ -210,7 +210,7 @@ class ViewController: NSViewController, ProcessListViewControllerDelegate, NSApp
 #endif
         for runningApplication in runningApplications {
             if runningApplication.bundleIdentifier != "org.safeexambrowser.SEBVerificator" &&
-                (debug && runningApplication.bundleIdentifier != "com.apple.dt.Xcode") {
+                !(debug && runningApplication.bundleIdentifier == "com.apple.dt.Xcode") {
                 if runningApplication.activationPolicy == .regular {
                     DDLogInfo("Running app \(runningApplication.bundleIdentifier ?? "no BundleID") (\(runningApplication.executableURL?.absoluteString ?? "no executable URL")) is visible in the Dock and should be quit.")
                     let success = runningApplication.terminate()
