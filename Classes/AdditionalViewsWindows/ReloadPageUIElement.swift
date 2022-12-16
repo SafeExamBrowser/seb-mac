82fd35af1f1acf2baf99283027976db0db1caa6a
Daniel R. Schneider <danschlet@users.noreply.github.com>
2022-12-16 11:57:19 +0100

    SEBMAC-474 Fixed reload key shortcut not enabled correctly according to reload settings and selected window (main/additional).

---
+++ Classes/AdditionalViewsWindows/ReloadPageUIElement.swift
@@ -0,0 +1,13 @@
+//
+//  ReloadPageUIElement.swift
+//  SafeExamBrowser
+//
+//  Created by Daniel Schneider on 16.12.22.
+//
+
+import Cocoa
+
+public class ReloadPageUIElement: NSObject {
+    
+    @objc public var enabled = true
+}
