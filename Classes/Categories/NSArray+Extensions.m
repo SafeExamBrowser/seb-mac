f23a56de8fb483d9df0990a9e2902330a8c9edcf
Daniel R. Schneider <danschlet@users.noreply.github.com>
2023-01-03 23:13:58 +0100

    SEBMAC-445 Fixed another case when SEB 3.1/3.1.1 crashed on iOS when configured via MDM.

---
+++ Classes/Categories/NSArray+Extensions.m
@@ -0,0 +1,40 @@
+//
+//  NSArray+Extensions.m
+//  SafeExamBrowser
+//
+//  Created by Daniel Schneider on 03.01.23.
+//
+
+#import "NSArray+Extensions.h"
+#import "NSDictionary+Extensions.h"
+
+@implementation NSArray (Extensions)
+
+- (BOOL) containsArray:(NSArray *)array
+{
+    for (id object in array) {
+        Class objectClass = [object superclass];
+        
+        if (objectClass == NSDictionary.class || objectClass == NSMutableDictionary.class) {
+            BOOL dictionaryExists = NO;
+            for (NSDictionary *dictionary in self) {
+                if ([dictionary containsDictionary:object]) {
+                    dictionaryExists = YES;
+                    break;
+                } else if ([object containsDictionary:dictionary]) {
+                    dictionaryExists = YES;
+                    break;
+                }
+            }
+            if (!dictionaryExists) {
+                return NO;
+            }
+
+        } else if (![self containsObject:object]) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+@end
