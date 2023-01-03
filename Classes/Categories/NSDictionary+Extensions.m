7e64a5897f283353ff6fc713ed7523855a58e56c
Daniel R. Schneider <danschlet@users.noreply.github.com>
2023-01-03 23:13:58 +0100

    SEBMAC-445 Fixed another case when SEB 3.1/3.1.1 crashed on iOS when configured via MDM.

--- Classes/Categories/NSDictionary+Extensions.m
+++ Classes/Categories/NSDictionary+Extensions.m
@@ -6,6 +6,8 @@
 //
 
 #import "NSDictionary+Extensions.h"
+#import "NSArray+Extensions.h"
+
 
 @implementation NSDictionary (Extensions)
 
@@ -29,6 +31,38 @@
     if (![self objectForKey:key]) {
         [self setMatchingValueInDictionary:dictionary forKey:key];
     }
+}
+
+- (BOOL) containsDictionary:(NSDictionary *)dictionary
+{
+    NSArray *allKeys = dictionary.allKeys;
+    for (id key in allKeys) {
+        id value = [dictionary objectForKey:key];
+        Class valueClass = [value superclass];
+        id object = [self objectForKey:key];
+        Class objectClass = [object superclass];
+        
+        if (valueClass == NSDictionary.class || valueClass == NSMutableDictionary.class) {
+            if (objectClass == NSDictionary.class || objectClass == NSMutableDictionary.class) {
+                if (![object containsDictionary:value]) {
+                    return NO;
+                }
+            } else {
+                return NO;
+            }
+        } else if (valueClass == NSArray.class || valueClass == NSMutableArray.class) {
+            if (objectClass == NSArray.class || objectClass == NSMutableArray.class) {
+                if (![object containsArray:value]) {
+                    return NO;
+                }
+            } else {
+                return NO;
+            }
+        } else if (![value isEqual:object]) {
+            return NO;
+        }
+    }
+    return YES;
 }
 
 @end
