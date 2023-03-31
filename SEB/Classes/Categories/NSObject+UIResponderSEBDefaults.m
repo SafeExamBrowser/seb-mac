192ff72410d5ff1c1a1874129a39113402c65ef6
M Persson <mteep@users.noreply.github.com>
2023-03-31 15:51:23 +0200

    Enable physical keyboard arrow keys in UIWebView.
    By adding a trivial default implementation of the (UIResponder) method canPerformAction:withSender: in a NSObject category.
    This allows the UIThreadSafeNode wrapping the active DOMNode in a UIWebView to forward arrow key movement, including selections.

---
+++ SEB/Classes/Categories/NSObject+UIResponderSEBDefaults.m
@@ -0,0 +1,18 @@
+//
+//  NSObject+UIResponderSEBDefaults.m
+//  SafeExamBrowser
+//
+//  Created by M Persson on 2023-03-31.
+//
+
+#import "NSObject+UIResponderSEBDefaults.h"
+
+@implementation NSObject (UIResponderSEBDefaults)
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
+    BOOL result = [self respondsToSelector:action];
+    DDLogDebug(@"NSObject default canPerformAction: %s withSender: %@ => %s", sel_getName(action), sender, result ? "YES" : "NO");
+    return result;
+}
+
+@end
