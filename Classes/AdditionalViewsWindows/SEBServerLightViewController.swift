53f8dd9cba757363bc662b0c2cb1de9da67ca2d0
Daniel R. Schneider <danschlet@users.noreply.github.com>
2024-02-05 11:56:47 +0100

    SEBMAC-579 Added new settings for SEB Server Light discovery to InAppSettings (SEB Server page).

---
+++ Classes/AdditionalViewsWindows/SEBServerLightViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SEBServerLightViewController.swift
+//  Safe Exam Browser
+//
+//  Created by Daniel Schneider on 02.02.24.
+//
+
+import Foundation
+import Network
+
+class SEBServerLightViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate, NSWindowDelegate {
+    
+
+}
