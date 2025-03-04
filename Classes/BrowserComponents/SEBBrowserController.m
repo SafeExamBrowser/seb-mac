2fa9534bbfbf72addb6eebc77306586bdc73518c
Daniel R. Schneider <danschlet@users.noreply.github.com>
2025-03-04 22:04:08 +0100

    SEBMAC-616 Removed some legacy SEB versions from user agent (only using 3.4, 3.4.1 and 3.5). Updated Safari version in hardcoded user agent element strings.

--- Classes/BrowserComponents/SEBBrowserController.m
+++ Classes/BrowserComponents/SEBBrowserController.m
@@ -369,7 +369,7 @@ void run_block_on_ui_thread(dispatch_block_t block)
         }
 #endif
         // Add "SEB <version number>" to the browser's user agent, so the LMS SEB plugins recognize us
-        overrideUserAgent = [overrideUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@/%@ %@/3.2.5 %@/3.3 %@/3.3.2 %@/3.3.3 %@/3.4 %@/3.4.1 %@", SEBUserAgentDefaultSuffix, versionString, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, browserUserAgentSuffix]];
+        overrideUserAgent = [overrideUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@/%@ %@/3.4 %@/3.4.1 %@", SEBUserAgentDefaultSuffix, versionString, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, browserUserAgentSuffix]];
         _customSEBUserAgent = overrideUserAgent;
     }
     return _customSEBUserAgent;
