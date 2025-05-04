796653e0ab8e2f9871ed92f64762fbe80793646e
Daniel R. Schneider <danschlet@users.noreply.github.com>
2025-05-04 21:40:37 +0200

    SEBMAC-735 Increased version also for iOS to  SEB 3.5.1 . Added SEB 3.5 to version user agent strings.

--- Classes/BrowserComponents/SEBBrowserController.m
+++ Classes/BrowserComponents/SEBBrowserController.m
@@ -374,7 +374,7 @@ void run_block_on_ui_thread(dispatch_block_t block)
         }
 #endif
         // Add "SEB <version number>" to the browser's user agent, so the LMS SEB plugins recognize us
-        overrideUserAgent = [overrideUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@/%@ %@/3.4 %@/3.4.1 %@", SEBUserAgentDefaultSuffix, versionString, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, browserUserAgentSuffix]];
+        overrideUserAgent = [overrideUserAgent stringByAppendingString:[NSString stringWithFormat:@" %@/%@ %@/3.4 %@/3.4.1 %@/3.5 %@", SEBUserAgentDefaultSuffix, versionString, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, SEBUserAgentDefaultSuffix, browserUserAgentSuffix]];
         _customSEBUserAgent = overrideUserAgent;
     }
     return _customSEBUserAgent;
