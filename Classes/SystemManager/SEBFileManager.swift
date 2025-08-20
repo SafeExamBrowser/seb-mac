1ee2451dfc46da931269c38a9b0c2c122eddc302
Daniel R. Schneider <schneider@let.ethz.ch>
2025-08-20 11:40:32 +0200

    Updated project.

--- Classes/SystemManager/SEBFileManager.swift
+++ Classes/SystemManager/SEBFileManager.swift
@@ -3,6 +3,34 @@
 //  SafeExamBrowser
 //
 //  Created by Daniel Schneider on 19.08.2024.
+//  Copyright (c) 2010-2025 Daniel R. Schneider, ETH Zurich, IT Services,
+//  based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen
+//  Project concept: Thomas Piendl, Daniel R. Schneider, Damian Buechel,
+//  Andreas Hefti, Nadim Ritter,
+//  Tobias Halbherr, Kristina Isacson Wildi, Tony Moser,
+//  Marco Lehre, Dirk Bauer, Kai Reuter, Karsten Burger,
+//  Brigitte Schmucki, Oliver Rahs. French localization: Nicolas Dunand
+//
+//  ``The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//  http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations
+//  under the License.
+//
+//  The Original Code is Safe Exam Browser for Mac OS X.
+//
+//  The Initial Developer of the Original Code is Daniel R. Schneider.
+//  Portions created by Daniel R. Schneider are Copyright
+//  (c) 2010-2025 Daniel R. Schneider, ETH Zurich, IT Services,
+//  based on the original idea of Safe Exam Browser
+//  by Stefan Schneider, University of Giessen. All Rights Reserved.
+//
+//  Contributor(s): ______________________________________.
 //
 
 import Foundation
