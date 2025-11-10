d714534537db9454c39f705c277b774b463948b5
Daniel R. Schneider <schneider@let.ethz.ch>
2025-11-10 14:16:55 +0100

    SEBMAC-769 Fixed crash in CocoaLumberjack by removing one parameter in the Verbose log command: Could affect other formatted DDLog calls!

--- Classes/BrowserComponents/SEBAbstractWebView.m
+++ Classes/BrowserComponents/SEBAbstractWebView.m
@@ -690,7 +690,6 @@ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NS
 {
     NSURLRequest *request = navigationAction.request;
     NSURL *url = request.URL;
-//    DDLogVerbose(@"[SEBAbstractWebView decidePolicyForNavigationAction: %@ newTab: %hhd configuration:%@ downloadFilename:%@]: request = %@, URL = %@", navigationAction, newTab, configuration, downloadFilename, request, url);
     DDLogVerbose(@"[SEBAbstractWebView decidePolicyForNavigationAction: newTab: %hhd configuration:%@ downloadFilename:%@]: request = %@, URL = %@", newTab, configuration, downloadFilename, request, url);
     WKNavigationType navigationType = navigationAction.navigationType;
     NSString *httpMethod = request.HTTPMethod;
